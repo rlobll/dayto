ec800e5e05f30b365748a95c12fe6fbdf2e3918e
dmsql <dmsql3335@gmail.com>
2024-03-20 11:15:54 +0900

    네이버 리다이렉트주소 변경

--- src/page/login/naverLogin.js
+++ src/page/login/naverLogin.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Naver = () => {
   const NAVER_CLIENT_ID = "fpsR1qbI1E8CO2t8ATql"; // 발급받은 클라이언트 아이디
-  const REDIRECT_URL = "http://localhost:3000"; // callback URL
+  const REDIRECT_URL = "https://dayto.vercel.app/"; // callback URL
   const STATE = "false";
   const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URL}`;
 
